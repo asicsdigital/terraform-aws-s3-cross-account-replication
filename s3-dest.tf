4847bc6a0a8475823eab037417fe77f46d798729
John Noss <john.noss@runkeeper.com>
2018-09-19 14:35:56 -0400

    feat: remove unnecessary dest bucket policy statment

--- s3-dest.tf
+++ s3-dest.tf
@@ -22,27 +22,6 @@ data "aws_iam_policy_document" "dest_bucket_policy" {
       ]
     }
   }
-
-  statement {
-    sid = "2"
-
-    actions = [
-      "s3:GetBucketVersioning",
-      "s3:PutBucketVersioning",
-    ]
-
-    resources = [
-      "${local.dest_bucket_arn}",
-    ]
-
-    principals {
-      type = "AWS"
-
-      identifiers = [
-        "${local.source_root_user_arn}",
-      ]
-    }
-  }
 }
 
 resource "aws_s3_bucket" "dest" {
