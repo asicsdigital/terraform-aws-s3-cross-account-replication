191a4626d0cabbc74b1e8de28576b8dd81819b93
John Noss <john.noss@runkeeper.com>
2018-09-19 11:29:24 -0400

    feat: add iam user and encrypt key to keybase user

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,17 @@
+# Destination Account ID
+
 output "dest_account_id" {
   value = "${data.aws_caller_identity.dest.account_id}"
+}
+
+# Source write IAM user
+
+output "source_write_iam_user_access_key_id" {
+  value = "${aws_iam_access_key.source_write.id}"
+}
+
+# to decrypt:
+# echo -n "<THE ENCRYPTED SECRET>" | base64 --decode | keybase pgp decrypt
+output "source_write_iam_user_encrypted_secret_access_key" {
+  value = "${aws_iam_access_key.source_write.encrypted_secret}"
 }
